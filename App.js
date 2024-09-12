f666f26589e2e293de6aab262c55457d5f4fa518
Badal Patidar <badalpatidar210@gmail.com>
2024-09-12 13:38:32 +0530

    ignited the app

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
 
 // console.log(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm a H1 tag"),
+      React.createElement("h1", {}, "This is Namaste React"),
       React.createElement("h2", {}, "I'm a H2 tag"),
     ]),
     React.createElement("div", { id: "child" }, [
---
+++ README.md
@@ -0,0 +1,21 @@
+@
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hpt Module Replacement 
+- File Watching Algorithm - Written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of Files
+- Bundling 
+- Compress the files
+- Consistent Hashing
+- Code Splitting 
+- Differential Bundling - support older browsers
+- Daignostic
+- Error Handling
+- Host app on HTTPs
+- Tree Shaking - remove unused code for me 
+- Different dev and product bundles
